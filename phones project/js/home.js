a074edc16920e6539ada15564697f3490a6fac3f
AyhamGhader <117582760+AyhamGhader@users.noreply.github.com>
2022-11-08 05:47:47 +0200

    Add files via upload

---
+++ phones project/js/home.js
@@ -0,0 +1,22 @@
+
+let price1 = document.getElementById("price1")
+price1.innerHTML ="800.00"
+
+let price2 = document.getElementById("price2")
+price2.innerHTML ="159.00"
+
+let price3 = document.getElementById("price3")
+price3.innerHTML ="259.99"
+
+let numbers1 = document.getElementById("numbers1")
+numbers1.innerHTML="0934332056"
+
+let numbers2 = document.getElementById("numbers2")
+numbers2.innerHTML="0348591555"
+
+let numbers3 = document.getElementById("numbers3")
+numbers3.innerHTML="2115355"
+
+let numbers4 = document.getElementById("numbers4")
+numbers4.innerHTML="2155599"
+
